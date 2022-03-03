aaf5f22cd1525d24c97ecae2f3fd791a1107c5d6
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-03 22:43:47 +0000

    Red Test string Calculator

---
+++ Testing.Katas.Example.NUnit/StringCalculatorKatacs.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Testing.Katas.Example.NUnit
+{
+    public class StringCalculatorKatacs
+    {
+
+        [Test]
+        [TestCase("", 0)]
+        private void A_simple_string_will_return_0(string input, int expectation)
+        {
+            StringCalculator Calculator = new StringCalculator();
+            var result = Calculator.Add(input);
+
+            Assert.Equals(result, expectation);
+
+        }
+
+    }
+}
